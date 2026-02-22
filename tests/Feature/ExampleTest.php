16f18f620e915d15f600f92256cc50820b95a592
Kieran Brown <kswb96@gmail.com>
2026-02-22 01:45:04 +0000

    chore: delete placeholder ExampleTest.php files

--- tests/Feature/ExampleTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Laraworker\LaraworkerServiceProvider;
-
-test('service provider is registered', function () {
-    expect($this->app->getProviders(LaraworkerServiceProvider::class))
-        ->not->toBeEmpty();
-});
